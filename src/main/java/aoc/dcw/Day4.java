223821f031cc7a0c55f09f0b3dd96106b86375e1
dwatson <dwatson@ctic.us>
2024-12-04 09:02:02 -0500

    day 4 formatting

--- src/main/java/aoc/dcw/Day4.java
+++ src/main/java/aoc/dcw/Day4.java
@@ -19,21 +19,22 @@ public class Day4 {
         int foundXmas = 0;
         int foundMas = 0;
 
-        for(int y=0;y<length;y++){
-            for(int x=0;x<length;x++){
+        for (int y = 0; y < length; y++) {
+
+            for (int x = 0; x < length; x++) {
 
                 // Search in all directions starting from X
                 for (int yd = -1; yd <= 1; yd++) {
                     for (int xd = -1; xd <= 1; xd++) {
-                        if(searchXmas(x,y,0,xd,yd)) foundXmas ++;
+                        if (searchXmas(x, y, 0, xd, yd)) foundXmas++;
                     }
                 }
 
                 // Look for diagonals only, starting from 'A' index and going out
                 int startIndex = 1;
-                if( (searchMas(x,y,startIndex,1,1,1) && searchMas(x,y,startIndex,-1,-1,-1) ) || searchMas(x,y,startIndex,1,1,-1) && searchMas(x,y,startIndex,-1,-1,1) ) {
+                if ((searchMas(x, y, startIndex, 1, 1, 1) && searchMas(x, y, startIndex, -1, -1, -1)) || searchMas(x, y, startIndex, 1, 1, -1) && searchMas(x, y, startIndex, -1, -1, 1)) {
                     // Found SE diagonal
-                    if((searchMas(x,y,startIndex,-1,1,1) && searchMas(x,y,startIndex,1,-1,-1) || searchMas(x,y,startIndex,-1,1,-1) && searchMas(x,y,startIndex,1,-1,1)  )) {
+                    if ((searchMas(x, y, startIndex, -1, 1, 1) && searchMas(x, y, startIndex, 1, -1, -1) || searchMas(x, y, startIndex, -1, 1, -1) && searchMas(x, y, startIndex, 1, -1, 1))) {
                         // Found SW diagonal
                         foundMas++;
                     }
@@ -41,25 +42,23 @@ public class Day4 {
             }
         }
 
-        System.out.println("foundXmas: "+foundXmas);
-        System.out.println("foundMas: "+foundMas);
+        System.out.println("foundXmas: " + foundXmas);
+        System.out.println("foundMas: " + foundMas);
     }
 
     // Recursive search in a specific direction
     boolean searchXmas(int x, int y, int charIndex, int xDir, int yDir) {
-        if(charIndex == xmas.length()) return true;
+        if (charIndex == xmas.length()) return true;
         else if (y > -1 && y < length && x > -1 && x < length && lines[y].charAt(x) == xmas.charAt(charIndex)) {
             return searchXmas(x + xDir, y + yDir, charIndex + 1, xDir, yDir);
-        }
-        else return false;
+        } else return false;
     }
 
     // Recursive search in a specific x,y, and character index direction
-    boolean searchMas(int x, int y, int charIndex,int xDir, int yDir,  int charDir) {
-        if(charIndex == mas.length() || charIndex == -1) return true;
+    boolean searchMas(int x, int y, int charIndex, int xDir, int yDir, int charDir) {
+        if (charIndex == mas.length() || charIndex == -1) return true;
         else if (y > -1 && y < length && x > -1 && x < length && lines[y].charAt(x) == mas.charAt(charIndex)) {
             return searchMas(x + xDir, y + yDir, charIndex + charDir, xDir, yDir, charDir);
-        }
-        else return false;
+        } else return false;
     }
 }
