919125dbac7ccdff4e015da196004f3b39dbac0a
dwatson <dwatson@ctic.us>
2024-12-09 10:07:44 -0500

    day 9 cleanup

--- src/main/java/aoc/dcw/Day9.java
+++ src/main/java/aoc/dcw/Day9.java
@@ -42,12 +42,11 @@ public class Day9 {
         return new FileDisk(blocks);
     }
 
-    // Reorder the individual blocks
+    // Reorder the individual blocks based on part 1 instructions
     public List<Long> reorderBlocks(FileDisk disk) {
         BlockDisk blockDisk = disk.getBlockDisk();
         List<Long> blocks = blockDisk.blocks;
         logger.debug("reordering {}", blockDisk.blocks.size());
-        //int lastNonSpaceIndex = lastNonSpace(blocks, blocks.size() - 1);
         int lastNonSpaceIndex = blockDisk.lastNonSpace(blocks.size() - 1);
         logger.debug("lastNonSpace: {}", lastNonSpaceIndex);
 
@@ -61,11 +60,11 @@ public class Day9 {
         return blocks;
     }
 
-    // reorder the files
+    // reorder the files based on part 2 instructions
     public FileDisk reorderFiles(FileDisk disk) {
-        List<FileBlock> files = new ArrayList<>(disk.blocks);
+        List<FileBlock> files = new ArrayList<>(disk.files);
         FileDisk reorderedDisk = new FileDisk(files);
-        List<FileBlock> nonSpaceFilesReverse = new ArrayList<>(disk.blocks.stream().filter(f -> !f.isSpace()).toList());
+        List<FileBlock> nonSpaceFilesReverse = new ArrayList<>(disk.files.stream().filter(f -> !f.isSpace()).toList());
         Collections.reverse(nonSpaceFilesReverse);
         do {
             FileBlock file = nonSpaceFilesReverse.get(0);
@@ -87,22 +86,23 @@ public class Day9 {
 
 
     public static String FilesToString(Stream<FileBlock> blocks) {
-        return BlocksToString(blocks.flatMap(fb -> fb.getBlocks().stream()));
+        return BlocksToString(blocks.flatMap(FileBlock::getBlocks));
     }
 
     public static String BlocksToString(Stream<Long> blocks) {
         return blocks.map(i -> i == SPACE_ID ? SPACE : i.toString()).collect(Collectors.joining());
     }
 
-    // Class to manage a list of individual blocks
+    // Class to manage a list of individual blocks, regardless of the file id
     public static class BlockDisk {
 
-        final List<Long> blocks;
+        final List<Long> blocks; // each value is a file id or -1
 
         public BlockDisk(List<Long> blocks) {
             this.blocks = blocks;
         }
 
+        // find last block that is not a space
         public int lastNonSpace(int from) {
             for (int i = from; i >= 0; i--) {
                 if (blocks.get(i) != SPACE_ID) {
@@ -113,42 +113,45 @@ public class Day9 {
         }
     }
 
-    // Class to manage a list of file or space blocks
+    // Class to manage a list of contiguous file blocks
     public static class FileDisk {
 
-        public final List<FileBlock> blocks;
+        public final List<FileBlock> files; // each object is file / empty space + length
 
-        public FileDisk(List<FileBlock> blocks) {
-            this.blocks = blocks;
+        public FileDisk(List<FileBlock> files) {
+            this.files = files;
         }
 
-        // Convert the list of files to list of individual blocks
+        // Convert the disk of files to disk of individual blocks
         public BlockDisk getBlockDisk() {
-            return new BlockDisk(new ArrayList<>(blocks.stream().flatMap(fb -> fb.getBlocks().stream()).toList()));
+            return new BlockDisk(new ArrayList<>(files.stream().flatMap(FileBlock::getBlocks).toList()));
         }
 
-        public static long calcCheckSum(Stream<Long> ids) {
-            MutableInt blockId = new MutableInt(0);
-            return ids.map(id -> {
-                long bid = blockId.getAndIncrement();
-                return id == SPACE_ID ? 0 : id * bid;
-            }).reduce(Long::sum).orElseThrow();
-        }
-
-        public String toString() {
-            return BlocksToString(blocks.stream().flatMap(fb -> fb.getBlocks().stream()));
-        }
-
+        // checksum of the current list of blocks
         public long calcCheckSum() {
-            return calcCheckSum(blocks.stream().flatMap(fb -> Arrays.stream(Utilities.fill(fb.size, fb.fileId)).boxed()));
+            return calcCheckSum(files.stream().flatMap(fb -> Arrays.stream(Utilities.fill(fb.size, fb.fileId)).boxed()));
         }
 
+        // Find a space file that is at least a certain size
         public int findSpaceOfSize(int size) {
-            for (int i = 0; i < blocks.size(); i++) {
-                FileBlock block = blocks.get(i);
+            for (int i = 0; i < files.size(); i++) {
+                FileBlock block = files.get(i);
                 if (block.isSpace() && block.size >= size) return i;
             }
             return -1;
+        }
+
+        public String toString() {
+            return BlocksToString(files.stream().flatMap(FileBlock::getBlocks));
+        }
+
+        // Checksum of a list of file ids
+        public static long calcCheckSum(Stream<Long> fileIds) {
+            MutableInt blockId = new MutableInt(0);
+            return fileIds.map(id -> {
+                long bid = blockId.getAndIncrement();
+                return id == SPACE_ID ? 0 : id * bid;
+            }).reduce(Long::sum).orElseThrow();
         }
     }
 
@@ -166,8 +169,8 @@ public class Day9 {
             return StringUtils.repeat(fileId == SPACE_ID ? SPACE.charAt(0) : ("" + fileId).charAt(0), size);
         }
 
-        public List<Long> getBlocks() {
-            return Arrays.stream(Utilities.fill(size, fileId)).boxed().toList();
+        public Stream<Long> getBlocks() {
+            return Arrays.stream(Utilities.fill(size, fileId)).boxed();
         }
 
         public boolean isSpace() {
